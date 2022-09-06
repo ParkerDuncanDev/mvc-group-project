f3cf65d8692127eacf3247721ceb8477b48329f0
JP <johnpaulmitra@yahoo.com>
2022-09-06 17:41:21 -0500

    removed console.log for production

--- controllers/todos.js
+++ controllers/todos.js
@@ -8,7 +8,7 @@ module.exports = {
         try{
             const todoItems = await Todo.find({userId:req.user.id})
             const sharedTodoItems = await Todo.find({ sharedWith: `${req.user.id}` })
-            console.log(sharedTodoItems)
+            // console.log(sharedTodoItems)
             const itemsLeft = await Todo.countDocuments({userId:req.user.id,completed: false})
             const sharedItemsLeft = await Todo.countDocuments({sharedWith:req.user.id,completed: false})
             res.render('todos.ejs', {todos: todoItems,
